6dcb922a6377c2b164fa8244f432a0e1076576a9
Gchasing <541034902@qq.com>
2020-08-06 18:25:23 +0800

    add the drone_new files which include the eight shape circle

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/chasing/drone/src/init_trial
+# Build directory: /home/chasing/drone/src/init_trial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rl_launch")
+subdirs("rl_navigation")
---
+++ build/rl_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chasing/drone/src/init_trial/rl_navigation
+# Build directory: /home/chasing/drone/src/init_trial/build/rl_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
